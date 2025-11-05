2b09af555f274d906a63b2887246f66dcd087f6e
Ahmed4Magdy <Ahmed@example.com>
2025-11-06 00:40:46 +0200

    update sec

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.example.demo.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,70 +22,50 @@ public class SecurityConfig {
     private CustomUserDetailsService customUserDetailsService;
 
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-////                .userDetailsService(customUserDetailsService)//link between ui and security ...انا حطيط ده علشان سواجر مبيعرفش يلقط تلقاءي ويشوف الداتا من الداتابيز لانه بيتعمل مع يو اي ع عكس البوست مان اسبرينج بيلقطها اوتوماتيك
-//                .authorizeHttpRequests(auth -> auth
-//
-//                        .requestMatchers(HttpMethod.POST, "/user/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.PUT, "/user/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/user/{id}").hasAnyRole("ADMIN", "CUSTOMER")
-//
-//
-//                        .requestMatchers(HttpMethod.POST, "/monthly/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/monthly/**").hasAnyRole("ADMIN", "CUSTOMER")
-//
-//
-//                        .requestMatchers(HttpMethod.POST, "/transaction/**").hasAnyRole("ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/transaction/account/{accountId}").hasAnyRole("ADMIN", "CUSTOMER")
-//                        .requestMatchers(HttpMethod.DELETE, "/transaction/**").hasAnyRole("ADMIN")
-//
-//                        //Account
-//                        .requestMatchers(HttpMethod.POST, "/account/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/account").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/account/{account_id}").hasAnyRole("ADMIN", "CUSTOMER")
-//                        .requestMatchers(HttpMethod.GET, "/account/byuser/{userId}").hasAnyRole("ADMIN", "CUSTOMER")
-//                        .requestMatchers(HttpMethod.PUT, "/account/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/account/**").hasRole("ADMIN")
-//
-//                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//
-////                        .requestMatchers(HttpMethod.GET)
-//                        .anyRequest().authenticated()
-//        )
-//                .httpBasic(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
-//
-//
-//    // AuthenticationManager الجديد في Spring Security 6
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-//
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+//                .userDetailsService(customUserDetailsService)//link between ui and security ...انا حطيط ده علشان سواجر مبيعرفش يلقط تلقاءي ويشوف الداتا من الداتابيز لانه بيتعمل مع يو اي ع عكس البوست مان اسبرينج بيلقطها اوتوماتيك
+                .authorizeHttpRequests(auth -> auth
+
+                                .requestMatchers("/user/register","/user/login").permitAll()
+
+                                .requestMatchers(HttpMethod.POST, "/user/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/user/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/user/{id}").hasAnyRole("ADMIN", "CUSTOMER")
+
+
+                                .requestMatchers(HttpMethod.POST, "/monthly/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/monthly/**").hasAnyRole("ADMIN", "CUSTOMER")
+
+
+                                .requestMatchers(HttpMethod.POST, "/transaction/**").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/transaction/account/{accountId}").hasAnyRole("ADMIN", "CUSTOMER")
+                                .requestMatchers(HttpMethod.DELETE, "/transaction/**").hasAnyRole("ADMIN")
+
+                                //Account
+                                .requestMatchers(HttpMethod.POST, "/account/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/account").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/account/{account_id}").hasAnyRole("ADMIN", "CUSTOMER")
+                                .requestMatchers(HttpMethod.GET, "/account/byuser/{userId}").hasAnyRole("ADMIN", "CUSTOMER")
+                                .requestMatchers(HttpMethod.PUT, "/account/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/account/**").hasRole("ADMIN")
+
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
+//                        .requestMatchers(HttpMethod.GET)
+                                .anyRequest().authenticated()
+                )
+//                .httpBasic(Customizer.withDefaults());
+                .httpBasic(httpBasic -> httpBasic.disable());
+
         return http.build();
     }
 
 
-
-        // AuthenticationManager الجديد في Spring Security 6
+    // AuthenticationManager الجديد في Spring Security 6
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
@@ -94,6 +76,29 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+//        return http.build();
+//    }
+//
+//
+//
+//        // AuthenticationManager الجديد في Spring Security 6
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
+//
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
 }
 
